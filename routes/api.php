b03986986889893686531322c8ef2cdd13f2fbd0
Ferre Vangenechten <r0794129@student.thomasmore.be>
2022-01-26 10:07:14 +0100

    fix routes order

--- routes/api.php
+++ routes/api.php
@@ -71,9 +71,9 @@ Route::get('types', 'App\Http\Controllers\GraphTypeController@index');
 
 // WEATHER STATION UPDATE
 Route::get('stationupdates', 'App\Http\Controllers\WeatherStationUpdateController@index');
-Route::get('stationupdates/{station_id}/{update_id}', 'App\Http\Controllers\WeatherStationUpdateController@show');
 Route::get('stationupdates/update/{update_id}', 'App\Http\Controllers\WeatherStationUpdateController@specificUpdate');
 Route::get('stationupdates/station/{station_id}', 'App\Http\Controllers\WeatherStationUpdateController@specificStation');
+Route::get('stationupdates/{station_id}/{update_id}', 'App\Http\Controllers\WeatherStationUpdateController@show');
 Route::post('stationupdates', 'App\Http\Controllers\WeatherStationUpdateController@store');
 Route::delete('stationupdates/{stationUpdate}', 'App\Http\Controllers\WeatherStationUpdateController@delete');
 
