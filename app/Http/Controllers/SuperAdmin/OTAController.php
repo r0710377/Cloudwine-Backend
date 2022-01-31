df9060c536dab3515db90f7d9f0000d79706cf77
Ferre Vangenechten <r0794129@student.thomasmore.be>
2022-01-31 12:21:15 +0100

    update validator

--- app/Http/Controllers/SuperAdmin/OTAController.php
+++ app/Http/Controllers/SuperAdmin/OTAController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller as Controller;
 
 use App\Models\OTA_Update;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OTAController extends Controller
 {
@@ -15,8 +16,21 @@ class OTAController extends Controller
 
     public function store(Request $request)
     {
-        $update = OTA_Update::create($request->all());
+        $validator = Validator::make($request->all(), [
+            'bin_file_path' => 'required|string',
+            'name' => 'string|between:2,100|nullable',
+            'deploy_on' => 'required|date_format:d-m-Y H:i:s',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        if($validator->validated()){
+            $update = OTA_Update::create($request->all());
+        }
         return response()->json($update, 201); //201 --> Object created. Usefull for the store actions
+
     }
 
     public function show(OTA_Update $update)
@@ -26,7 +40,20 @@ class OTAController extends Controller
 
     public function update(Request $request, OTA_Update $update)
     {
-        $update->update($request->all());
+        $validator = Validator::make($request->all(), [
+            'bin_file_path' => 'required|string',
+            'name' => 'string|between:2,100|nullable',
+            'deploy_on' => 'required|date_format:d-m-Y H:i:s',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        if($validator->validated()){
+            $update->update($request->all());
+        }
+        
         return response()->json($update,200); //200 --> OK, The standard success code and default option
     }
 
